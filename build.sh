0dd052f70a29aef13fbf4b6dfdbc37c2024aabb0
Auston Stewart <auston@shedbuilt.net>
2018-01-11 19:46:45 -0500

    Adding toolchain and bootstrap modes, patching glob exploits

--- build.sh
+++ build.sh
@@ -1,27 +1,55 @@
 #!/bin/bash
 # If /usr/include/limits.h is present when building, make enters an infinite loop in glibc 2.26
 # HACK: For now, temporarily move the installed /usr/include/limits.h aside when compiling
+
+shed_glibc_cleanup ()
+{
+    # HACK: Move back /usr/include/limits.h following compilation
+    if [ -e /usr/include/limits.h.bak ]; then
+        mv /usr/include/limits.h.bak /usr/include/limits.h
+    fi
+}
+
 if [ -e /usr/include/limits.h ]; then
     mv /usr/include/limits.h /usr/include/limits.h.bak
 fi
-patch -Np1 -i "${SHED_PATCHDIR}/glibc-2.26-fhs-1.patch"
-GCC_INCDIR=/usr/lib/gcc/$(gcc -dumpmachine)/7.2.0/include
+
+# Configure
 mkdir -v build
 cd build
-CC="gcc -isystem $GCC_INCDIR -isystem /usr/include" \
-../configure --prefix=/usr                          \
-             --disable-werror                       \
-             --enable-kernel=3.2                    \
-             --enable-stack-protector=strong        \
-             libc_cv_slibdir=/lib
-unset GCC_INCDIR
-make -j $SHED_NUMJOBS
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ../configure --prefix=/tools                    \
+                     --host=$SHED_TARGET                \
+                     --build=$(../scripts/config.guess) \
+                     --enable-kernel=3.2                \
+                     --with-headers=/tools/include      \
+                     libc_cv_forced_unwind=yes          \
+                     libc_cv_c_cleanup=yes || ( shed_glibc_cleanup && return 1 )
+        ;;
+    bootstrap)
+        # Avoid references to the temporary /tools directory
+        ln -sfv /tools/lib/gcc /usr/lib
+        ;&
+    *)
+        patch -Np1 -i "${SHED_PATCHDIR}/glibc-2.26-fhs-1.patch"
+        patch -Np1 -i "${SHED_PATCHDIR}/glibc-2.26-local_glob_exploits-2.patch"
+        GCC_INCDIR=/usr/lib/gcc/$(gcc -dumpmachine)/7.2.0/include
+        CC="gcc -isystem $GCC_INCDIR -isystem /usr/include" \
+        ../configure --prefix=/usr                          \
+                     --disable-werror                       \
+                     --enable-kernel=3.2                    \
+                     --enable-stack-protector=strong        \
+                     libc_cv_slibdir=/lib || ( shed_glibc_cleanup && return 1 )
+        unset GCC_INCDIR
+        ;;
+esac
+
+# Build
+make -j $SHED_NUMJOBS || ( shed_glibc_cleanup && return 1 )
 sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile
-make "DESTDIR=${SHED_FAKEROOT}" install
-# HACK: Move back /usr/include/limits.h following compilation
-if [ -e /usr/include/limits.h.bak ]; then
-    mv /usr/include/limits.h.bak /usr/include/limits.h
-fi
+make DESTDIR="$SHED_FAKEROOT" install || ( shed_glibc_cleanup && return 1 )
+shed_glibc_cleanup
 
 # Install ncsd config files
 install -v -Dm644 ../nscd/nscd.conf "${SHED_FAKEROOT}/etc/nscd.conf"
@@ -30,9 +58,11 @@ install -v -Dm644 ../nscd/nscd.service "${SHED_FAKEROOT}/lib/systemd/system/nscd
 mkdir -pv "${SHED_FAKEROOT}/var/cache/nscd"
 mkdir -pv "${SHED_FAKEROOT}/usr/lib/locale"
 mkdir -v "${SHED_FAKEROOT}/etc"
-# Install default config files
+
+# Install other default config files
 install -v -m644 "${SHED_CONTRIBDIR}/nsswitch.conf" "${SHED_FAKEROOT}/etc/nsswitch.default"
 install -v -m644 "${SHED_CONTRIBDIR}/ld.so.conf" "${SHED_FAKEROOT}/etc/ld.so.default"
 mkdir -pv "${SHED_FAKEROOT}/etc/ld.so.conf.d"
+
 # Compatibility symlink for non ld-linux-armhf awareness
 ln -sv ld-2.26.so "${SHED_FAKEROOT}/lib/ld-linux.so.3"
