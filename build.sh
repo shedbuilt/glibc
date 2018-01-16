163640179d65f0a089a6cea97d3706d09b1cbb85
Auston Stewart <auston@shedbuilt.net>
2018-01-15 15:53:34 -1000

    Fixing the toolchain target.

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ cd build
 case "$SHED_BUILDMODE" in
     toolchain)
         ../configure --prefix=/tools                    \
-                     --host=$SHED_TARGET                \
+                     --host=$SHED_TOOLCHAIN_TARGET      \
                      --build=$(../scripts/config.guess) \
                      --enable-kernel=3.2                \
                      --with-headers=/tools/include      \
@@ -53,18 +53,23 @@ sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile
 make DESTDIR="$SHED_FAKEROOT" install || { shed_glibc_cleanup; exit 1; }
 shed_glibc_cleanup
 
-# Install ncsd config files
-install -v -Dm644 ../nscd/nscd.conf "${SHED_FAKEROOT}/etc/nscd.conf"
-install -v -Dm644 ../nscd/nscd.tmpfiles "${SHED_FAKEROOT}/usr/lib/tmpfiles.d/nscd.conf"
-install -v -Dm644 ../nscd/nscd.service "${SHED_FAKEROOT}/lib/systemd/system/nscd.service"
-mkdir -pv "${SHED_FAKEROOT}/var/cache/nscd"
-mkdir -pv "${SHED_FAKEROOT}/usr/lib/locale"
-mkdir -v "${SHED_FAKEROOT}/etc"
+case "$SHED_BUILDMODE" in
+    bootstrap|release)
+        # Install ncsd config files
+        install -v -Dm644 ../nscd/nscd.conf "${SHED_FAKEROOT}/etc/nscd.conf"
+        install -v -Dm644 ../nscd/nscd.tmpfiles "${SHED_FAKEROOT}/usr/lib/tmpfiles.d/nscd.conf"
+        install -v -Dm644 ../nscd/nscd.service "${SHED_FAKEROOT}/lib/systemd/system/nscd.service"
+        mkdir -pv "${SHED_FAKEROOT}/var/cache/nscd"
+        mkdir -pv "${SHED_FAKEROOT}/usr/lib/locale"
+        mkdir -v "${SHED_FAKEROOT}/etc"
 
-# Install other default config files
-install -v -m644 "${SHED_CONTRIBDIR}/nsswitch.conf" "${SHED_FAKEROOT}/etc/nsswitch.default"
-install -v -m644 "${SHED_CONTRIBDIR}/ld.so.conf" "${SHED_FAKEROOT}/etc/ld.so.default"
-mkdir -pv "${SHED_FAKEROOT}/etc/ld.so.conf.d"
-
-# Compatibility symlink for non ld-linux-armhf awareness
-ln -sv ld-2.26.so "${SHED_FAKEROOT}/lib/ld-linux.so.3"
+        # Install other default config files
+        install -v -m644 "${SHED_CONTRIBDIR}/nsswitch.conf" "${SHED_FAKEROOT}/etc/nsswitch.default"
+        install -v -m644 "${SHED_CONTRIBDIR}/ld.so.conf" "${SHED_FAKEROOT}/etc/ld.so.default"
+        mkdir -pv "${SHED_FAKEROOT}/etc/ld.so.conf.d"
+        ;&
+    *)
+        # Compatibility symlink for non ld-linux-armhf awareness
+        ln -sv ld-2.26.so "${SHED_FAKEROOT}/lib/ld-linux.so.3"
+        ;;
+esac
