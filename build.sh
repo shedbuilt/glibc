2b5707e536ee0895c8659523d0c68d24e9b73361
Auston Stewart <auston@shedbuilt.net>
2018-05-22 16:59:09 -1000

    Updating for blank package options

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 shed_glibc_cleanup ()
 {
     # HACK: Move back /usr/include/limits.h following compilation
@@ -7,7 +6,7 @@ shed_glibc_cleanup ()
         mv /usr/include/limits.h.bak /usr/include/limits.h
     fi
 }
-
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 # If /usr/include/limits.h is present when building, make enters an infinite loop in glibc 2.26
 # HACK: For now, temporarily move the installed /usr/include/limits.h aside when compiling
 if [ -e /usr/include/limits.h ]; then
@@ -20,38 +19,36 @@ patch -Np1 -i "${SHED_PKG_PATCH_DIR}/glibc-2.27-fhs-1.patch" || exit 1
 # Configure
 mkdir -v build
 cd build
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        if [ "$SHED_BUILD_HOST" == 'toolchain' ]; then
-            ../configure --prefix=/tools                    \
-                         --host=$SHED_TOOLCHAIN_TARGET      \
-                         --build=$(../scripts/config.guess) \
-                         --enable-kernel=3.2                \
-                         --with-headers=/tools/include      \
-                         libc_cv_forced_unwind=yes          \
-                         libc_cv_c_cleanup=yes || { shed_glibc_cleanup; exit 1; }
-        else
-            echo "Unsupported host setting for toolchain build: '$SHED_BUILD_HOST'"
-            exit 1
-        fi
-        ;;
-    bootstrap)
-        # Avoid references to the temporary /tools directory
-        ln -sfv /tools/lib/gcc /usr/lib
-        # Deal with hard-coded path to m4
-        ln -sfv /tools/bin/m4 /usr/bin
-        ;&
-    *)
-        GCC_INCDIR="/usr/lib/gcc/${SHED_NATIVE_TARGET}/7.3.0/include"
-        CC="gcc -isystem $GCC_INCDIR -isystem /usr/include" \
-        ../configure --prefix=/usr                          \
-                     --disable-werror                       \
-                     --enable-kernel=3.2                    \
-                     --enable-stack-protector=strong        \
-                     libc_cv_slibdir=/lib || { shed_glibc_cleanup; exit 1; }
-        unset GCC_INCDIR
-        ;;
-esac
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[bootstrap]}" ]; then
+    # Avoid references to the temporary /tools directory
+    ln -sfv /tools/lib/gcc /usr/lib
+    # Deal with hard-coded path to m4
+    ln -sfv /tools/bin/m4 /usr/bin
+fi
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    if [ "$SHED_BUILD_HOST" != "$SHED_NATIVE_TARGET" ]; then
+        ../configure --prefix=/tools                    \
+                     --host=$SHED_BUILD_HOST            \
+                     --build=$(../scripts/config.guess) \
+                     --enable-kernel=3.2                \
+                     --with-headers=/tools/include      \
+                     libc_cv_forced_unwind=yes          \
+                     libc_cv_c_cleanup=yes || { shed_glibc_cleanup; exit 1; }
+    else
+        echo "Unsupported host setting for toolchain build: '$SHED_BUILD_HOST'"
+        exit 1
+    fi
+else
+    # HACK: Explicit reference to versioned gcc directory creates a hard dependency
+    GCC_INCDIR="/usr/lib/gcc/${SHED_BUILD_TARGET}/8.1.0/include"
+    CC="gcc -isystem $GCC_INCDIR -isystem /usr/include" \
+    ../configure --prefix=/usr                          \
+                 --disable-werror                       \
+                 --enable-kernel=3.2                    \
+                 --enable-stack-protector=strong        \
+                 libc_cv_slibdir=/lib || { shed_glibc_cleanup; exit 1; }
+    unset GCC_INCDIR
+fi
 
 # Build
 make -j $SHED_NUM_JOBS || { shed_glibc_cleanup; exit 1; }
@@ -59,34 +56,31 @@ sed '/test-installation/s@$(PERL)@echo not running@' -i ../Makefile
 make DESTDIR="$SHED_FAKE_ROOT" install || { shed_glibc_cleanup; exit 1; }
 shed_glibc_cleanup
 
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        # Compatibility symlink for non ld-linux-armhf awareness
-        ln -sv ld-${SHED_PKG_VERSION}.so "${SHED_FAKE_ROOT}/tools/lib/ld-linux.so.3" || exit 1
-        ;;
-    *)
-        # Install ncsd config files
-        install -v -Dm644 ../nscd/nscd.conf "${SHED_FAKE_ROOT}/etc/nscd.conf" &&
-        install -v -Dm644 ../nscd/nscd.tmpfiles "${SHED_FAKE_ROOT}/usr/lib/tmpfiles.d/nscd.conf" &&
-        install -v -Dm644 ../nscd/nscd.service "${SHED_FAKE_ROOT}/lib/systemd/system/nscd.service" &&
-        mkdir -pv "${SHED_FAKE_ROOT}/var/cache/nscd" &&
-        mkdir -pv "${SHED_FAKE_ROOT}/usr/lib/locale" &&
-        mkdir -v "${SHED_FAKE_ROOT}/etc" &&
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    # Compatibility symlink for non ld-linux-armhf awareness
+    ln -sv ld-${SHED_PKG_VERSION}.so "${SHED_FAKE_ROOT}/tools/lib/ld-linux.so.3" || exit 1
+else
+    # Install ncsd config files
+    install -v -Dm644 ../nscd/nscd.conf "${SHED_FAKE_ROOT}/etc/nscd.conf" &&
+    install -v -Dm644 ../nscd/nscd.tmpfiles "${SHED_FAKE_ROOT}/usr/lib/tmpfiles.d/nscd.conf" &&
+    install -v -Dm644 ../nscd/nscd.service "${SHED_FAKE_ROOT}/lib/systemd/system/nscd.service" &&
+    mkdir -pv "${SHED_FAKE_ROOT}/var/cache/nscd" &&
+    mkdir -pv "${SHED_FAKE_ROOT}/usr/lib/locale" &&
+    mkdir -v "${SHED_FAKE_ROOT}/etc" &&
 
-        # Install other default config files
-        install -v -dm755 "${SHED_FAKE_ROOT}/usr/share/defaults/etc" &&
-        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/locale.conf" "${SHED_FAKE_ROOT}/usr/share/defaults/etc" &&
-        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/nsswitch.conf" "${SHED_FAKE_ROOT}/usr/share/defaults/etc" &&
-        install -v -m644 "${SHED_PKG_CONTRIB_DIR}/ld.so.conf" "${SHED_FAKE_ROOT}/usr/share/defaults/etc" &&
-        install -v -dm755 "${SHED_FAKE_ROOT}/etc/ld.so.conf.d" &&
+    # Install other default config files
+    install -v -dm755 "${SHED_FAKE_ROOT}/usr/share/defaults/etc" &&
+    install -v -m644 "${SHED_PKG_CONTRIB_DIR}/locale.conf" "${SHED_FAKE_ROOT}/usr/share/defaults/etc" &&
+    install -v -m644 "${SHED_PKG_CONTRIB_DIR}/nsswitch.conf" "${SHED_FAKE_ROOT}/usr/share/defaults/etc" &&
+    install -v -m644 "${SHED_PKG_CONTRIB_DIR}/ld.so.conf" "${SHED_FAKE_ROOT}/usr/share/defaults/etc" &&
+    install -v -dm755 "${SHED_FAKE_ROOT}/etc/ld.so.conf.d" &&
 
-        # Compatibility symlink for non ld-linux-armhf awareness
-        ln -sv ld-${SHED_PKG_VERSION}.so "${SHED_FAKE_ROOT}/lib/ld-linux.so.3" || exit 1
+    # Compatibility symlink for non ld-linux-armhf awareness
+    ln -sv ld-${SHED_PKG_VERSION}.so "${SHED_FAKE_ROOT}/lib/ld-linux.so.3" || exit 1
 
-        # 64-bit compatibility symlink
-        if [[ $SHED_NATIVE_TARGET =~ ^aarch64-.* ]]; then
-            mkdir -v "${SHED_FAKE_ROOT}/lib64" &&
-            ln -sfv ../lib/ld-linux-aarch64.so.1 "${SHED_FAKE_ROOT}/lib64" || exit 1
-        fi
-        ;;
-esac
+    # 64-bit compatibility symlink
+    if [[ $SHED_BUILD_TARGET =~ ^aarch64-.* ]]; then
+        mkdir -v "${SHED_FAKE_ROOT}/lib64" &&
+        ln -sfv ../lib/ld-linux-aarch64.so.1 "${SHED_FAKE_ROOT}/lib64" || exit 1
+    fi
+fi
--- postinstall.sh
+++ postinstall.sh
@@ -1,50 +1,46 @@
 #!/bin/bash
-case "$SHED_BUILD_MODE" in
-    toolchain)
-        echo "Error: post-install should not be performed for glibc in toolchain build mode."
-        exit 1
-        ;;
-    bootstrap)
-        # Adjust toolchain following glibc installation in bootstrap
-        mv -v /tools/bin/{ld,ld-old} &&
-        mv -v /tools/${SHED_NATIVE_TARGET}/bin/{ld,ld-old} &&
-        mv -v /tools/bin/{ld-new,ld} &&
-        ln -sv /tools/bin/ld /tools/${SHED_NATIVE_TARGET}/bin/ld &&
-        gcc -dumpspecs | sed -e 's@/tools@@g'                   \
-            -e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' \
-            -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' >      \
-                `dirname $(gcc --print-libgcc-file-name)`/specs || exit 1
-        ;&
-    *)
-        localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8 &&
-        localedef -i de_DE -f ISO-8859-1 de_DE &&
-        localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro &&
-        localedef -i de_DE -f UTF-8 de_DE.UTF-8 &&
-        localedef -i en_GB -f UTF-8 en_GB.UTF-8 &&
-        localedef -i en_HK -f ISO-8859-1 en_HK &&
-        localedef -i en_PH -f ISO-8859-1 en_PH &&
-        localedef -i en_US -f ISO-8859-1 en_US &&
-        localedef -i en_US -f UTF-8 en_US.UTF-8 &&
-        localedef -i es_MX -f ISO-8859-1 es_MX &&
-        localedef -i fa_IR -f UTF-8 fa_IR &&
-        localedef -i fr_FR -f ISO-8859-1 fr_FR &&
-        localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro &&
-        localedef -i fr_FR -f UTF-8 fr_FR.UTF-8 &&
-        localedef -i it_IT -f ISO-8859-1 it_IT &&
-        localedef -i it_IT -f UTF-8 it_IT.UTF-8 &&
-        localedef -i ja_JP -f EUC-JP ja_JP &&
-        localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R &&
-        localedef -i ru_RU -f UTF-8 ru_RU.UTF-8 &&
-        localedef -i tr_TR -f UTF-8 tr_TR.UTF-8 &&
-        localedef -i zh_CN -f GB18030 zh_CN.GB18030 || exit 1
-        if [ ! -e /etc/locale.conf ]; then
-            ln -sfv /usr/share/defaults/etc/locale.conf /etc/locale.conf || exit 1
-        fi
-        if [ ! -e /etc/nsswitch.conf ]; then
-            ln -sfv /usr/share/defaults/etc/nsswitch.conf /etc/nsswitch.conf || exit 1
-        fi
-        if [ ! -e /etc/ld.so.conf ]; then
-            ln -sfv /usr/share/defaults/etc/ld.so.default /etc/ld.so.conf || exit 1
-        fi
-        ;;
-esac
+declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
+if [ -n "${SHED_PKG_LOCAL_OPTIONS[toolchain]}" ]; then
+    echo "Error: post-install should not be performed for glibc in toolchain build mode."
+    exit 1
+elif [ -n "${SHED_PKG_LOCAL_OPTIONS[bootstrap]}" ]; then
+    # Adjust toolchain following glibc installation in bootstrap
+    mv -v /tools/bin/{ld,ld-old} &&
+    mv -v /tools/${SHED_BUILD_TARGET}/bin/{ld,ld-old} &&
+    mv -v /tools/bin/{ld-new,ld} &&
+    ln -sv /tools/bin/ld /tools/${SHED_BUILD_TARGET}/bin/ld &&
+    gcc -dumpspecs | sed -e 's@/tools@@g'                   \
+        -e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' \
+        -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' >      \
+            `dirname $(gcc --print-libgcc-file-name)`/specs || exit 1
+fi
+localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8 &&
+localedef -i de_DE -f ISO-8859-1 de_DE &&
+localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro &&
+localedef -i de_DE -f UTF-8 de_DE.UTF-8 &&
+localedef -i en_GB -f UTF-8 en_GB.UTF-8 &&
+localedef -i en_HK -f ISO-8859-1 en_HK &&
+localedef -i en_PH -f ISO-8859-1 en_PH &&
+localedef -i en_US -f ISO-8859-1 en_US &&
+localedef -i en_US -f UTF-8 en_US.UTF-8 &&
+localedef -i es_MX -f ISO-8859-1 es_MX &&
+localedef -i fa_IR -f UTF-8 fa_IR &&
+localedef -i fr_FR -f ISO-8859-1 fr_FR &&
+localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro &&
+localedef -i fr_FR -f UTF-8 fr_FR.UTF-8 &&
+localedef -i it_IT -f ISO-8859-1 it_IT &&
+localedef -i it_IT -f UTF-8 it_IT.UTF-8 &&
+localedef -i ja_JP -f EUC-JP ja_JP &&
+localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R &&
+localedef -i ru_RU -f UTF-8 ru_RU.UTF-8 &&
+localedef -i tr_TR -f UTF-8 tr_TR.UTF-8 &&
+localedef -i zh_CN -f GB18030 zh_CN.GB18030 || exit 1
+if [ ! -e /etc/locale.conf ]; then
+    ln -sfv /usr/share/defaults/etc/locale.conf /etc/locale.conf || exit 1
+fi
+if [ ! -e /etc/nsswitch.conf ]; then
+    ln -sfv /usr/share/defaults/etc/nsswitch.conf /etc/nsswitch.conf || exit 1
+fi
+if [ ! -e /etc/ld.so.conf ]; then
+    ln -sfv /usr/share/defaults/etc/ld.so.default /etc/ld.so.conf || exit 1
+fi
